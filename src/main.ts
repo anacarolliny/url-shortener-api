3c8623ea8172663085e16ef9700c558476c64110
anacarolliny <acss2011@hotmail.com>
2024-08-17 17:03:53 -0300

    chore:

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.get(ConfigService);
-
+  const config = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription('API for shortening URLs and managing authentication')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/modules/auth/dtos/login-dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @ApiProperty({
+    description: 'The email address of the user',
+    example: 'user@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: "The user's password",
+    example: 'password123',
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
